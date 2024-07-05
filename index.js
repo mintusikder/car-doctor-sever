94e00f68f735d24184ea0e62bf3101d1a1c2ac96
Mintu Sikder <mintusikder15@gmail.com>
2024-07-05 17:12:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmmjiwd.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const serverCollection = client.db("doctorCar").collection("services");
+    const bookingsCollection = client.db("doctorCar").collection("bookings");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serverCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        projection: { title: 1, price: 1, img: 1, service_id: 1, },
+      };
+
+      const result = await serverCollection.findOne(query,options);
+      res.send(result);
+    });
+
+    // booking
+
+    app.post("/bookings", async (req,res)=>{
+      const booking = req.body
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Car");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
